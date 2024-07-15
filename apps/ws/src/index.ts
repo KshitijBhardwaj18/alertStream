521f545526bc431039d478136e4889ea7155b491
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-07-15 09:38:39 +0530

    (fix:) configuref for cors error

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,4 +1,5 @@
 import * as WebSocket from 'ws';
+import * as http from 'http';
 import * as dotenv from 'dotenv';
 import * as jwt from 'jsonwebtoken';
 
@@ -6,7 +7,24 @@ dotenv.config();
 
 const port = 4000;
 
-const wss = new WebSocket.Server({ port: port });
+const server = http.createServer((req, res) => {
+  // Handle CORS for HTTP requests
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  res.writeHead(404);
+  res.end();
+});
+
+const wss = new WebSocket.Server({ server });
 
 interface User {
   email: string;
@@ -42,6 +60,25 @@ function updateUserList() {
 }
 
 wss.on('connection', (ws: WebSocket, req) => {
+  const params = new URLSearchParams(req.url?.split('?')[1]);
+  const token = params.get('token');
+
+  if (!token) {
+    ws.close(1008, 'Token not provided');
+    return;
+  }
+
+  let decoded: User;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET as string) as User;
+  } catch (err) {
+    console.log('Invalid token:', err);
+    ws.close(1008, 'Invalid token');
+    return;
+  }
+
+  const registeredUser: User = decoded;
+
   ws.on('message', (message: WebSocket.Data) => {
     const messageData = message.toString();
     const data = JSON.parse(messageData);
@@ -64,48 +101,6 @@ wss.on('connection', (ws: WebSocket, req) => {
         console.log('Unknown message type received');
     }
   });
-  const params = new URLSearchParams(req.url?.split('?')[1]);
-  const token = params.get('token');
-
-  if (!token) {
-    
-    ws.close(1008, 'Token not provided');
-    return;
-  }
-
-  let decoded: User;
-  try {
-    decoded = jwt.verify(token, "secret") as User;
-  } catch (err) {
-    console.log('Invalid token:', err);
-    ws.close(1008, 'Invalid token');
-    return;
-  }
-
-  const registeredUser: User = decoded;
-
-  // ws.on('message', (message: WebSocket.Data) => {
-  //   const messageData = message.toString();
-  //   const data = JSON.parse(messageData);
-  //   switch (data.type) {
-  //     case 'init':
-  //       users.set(registeredUser.id, { ws, user: registeredUser });
-  //       ws.send(JSON.stringify({ type: 'userId', userId: registeredUser.id }));
-  //       updateUserList();
-  //       break;
-  //     case 'ping':
-  //       if (registeredUser) {
-  //         if (data.target === 'all') {
-  //           broadcastPing(registeredUser.id, registeredUser.name);
-  //         } else {
-  //           sendPing(data.target, registeredUser.name);
-  //         }
-  //       }
-  //       break;
-  //     default:
-  //       console.log('Unknown message type received');
-  //   }
-  // });
 
   ws.on('close', () => {
     users.delete(registeredUser.id);
@@ -113,4 +108,6 @@ wss.on('connection', (ws: WebSocket, req) => {
   });
 });
 
-console.log('WebSocket server is running on port', port);
+server.listen(port, () => {
+  console.log('WebSocket server is running on port', port);
+});
